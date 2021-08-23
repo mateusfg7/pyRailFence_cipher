0a0766a2a059eef60a069d51664093d9fc9bf98b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-08-23 18:30:26 -0300

    format and return cipher message

--- main.py
+++ main.py
@@ -22,3 +22,9 @@ for letter in message:
     else:
         trail_2.append(letter)
         trail_switch = 1
+
+cipher_message = trail_1 + trail_2
+cipher_message_str = ''.join(cipher_message)
+formated_cipher_message = cipher_message_str.upper()
+
+print(f"\nMessage: {message}\nCipher: {formated_cipher_message}")
