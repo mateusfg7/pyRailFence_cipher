994a5bc0c44066c2afe4c33f1433bb263a457679
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-08-24 11:15:18 -0300

    make encode function independent of external variables

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ else:
     exit()
 
 
-def encode():
+def encode(string_to_encode):
     trail_1 = []
     trail_2 = []
     trail_switch = 1
 
-    for letter in message:
+    for letter in string_to_encode:
         if trail_switch == 1:
             trail_1.append(letter)
             trail_switch = 2
@@ -27,8 +27,7 @@ def encode():
 
     cipher_message = trail_1 + trail_2
     cipher_message_str = ''.join(cipher_message)
-    formated_cipher_message = cipher_message_str.upper()
 
-    return formated_cipher_message
+    return cipher_message_str.upper()
 
-print(f"\nMessage: {message}\nCipher: {encode()}")
+print(f"\nMessage: {message}\nCipher: {encode(message)}")
