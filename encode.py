8209219b7c042e351459818a10997b3dae74863f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-08-24 11:48:19 -0300

    update encode function structure

--- encode.py
+++ encode.py
@@ -1,18 +1,15 @@
 def encode(string_to_encode):
-    trail_1 = []
-    trail_2 = []
-    trail_switch = 1
+    trails = {
+        0: [],
+        1: []
+    }
+    curr_trail = 0
 
     for letter in string_to_encode:
-        if trail_switch == 1:
-            trail_1.append(letter)
-            trail_switch = 2
-        else:
-            trail_2.append(letter)
-            trail_switch = 1
+        trails[curr_trail].append(letter)
+        curr_trail = 0 if curr_trail != 0 else 1
 
-    cipher_message = trail_1 + trail_2
-    cipher_message_str = ''.join(cipher_message)
+    cipher_message = ''.join(trails[0] + trails[1])
 
-    return cipher_message_str.upper()
+    return cipher_message.upper()
 
