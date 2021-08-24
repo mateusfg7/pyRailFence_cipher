70f0e017fc325c3fd3cd4818cacd9eae4c431094
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-08-24 11:10:33 -0300

    create encode function

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ else:
     print("Pass the message to be encrypted using the option '-m'.")
     exit()
 
-trail_1 = []
-trail_2 = []
-trail_switch = 1
 
-for letter in message:
-    if trail_switch == 1:
-        trail_1.append(letter)
-        trail_switch = 2
-    else:
-        trail_2.append(letter)
-        trail_switch = 1
+def encode():
+    trail_1 = []
+    trail_2 = []
+    trail_switch = 1
 
-cipher_message = trail_1 + trail_2
-cipher_message_str = ''.join(cipher_message)
-formated_cipher_message = cipher_message_str.upper()
+    for letter in message:
+        if trail_switch == 1:
+            trail_1.append(letter)
+            trail_switch = 2
+        else:
+            trail_2.append(letter)
+            trail_switch = 1
 
-print(f"\nMessage: {message}\nCipher: {formated_cipher_message}")
+    cipher_message = trail_1 + trail_2
+    cipher_message_str = ''.join(cipher_message)
+    formated_cipher_message = cipher_message_str.upper()
+
+    return formated_cipher_message
+
+print(f"\nMessage: {message}\nCipher: {encode()}")
